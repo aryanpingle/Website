6b3903f8caec16cbf6e32523c4366946eed9ad3a
aryanpingle <realaryanpingle@gmail.com>
2026-01-26 00:50:12 +0530

    Minor refactoring + mask image

--- src/components/FrontpageGrid/FrontPageGrid.tsx
+++ src/components/FrontpageGrid/FrontPageGrid.tsx
@@ -1,9 +1,14 @@
 import { Masonry } from "@mui/lab";
-import image from "../../assets/processed/hero-1.png";
-import outerWildsImage from "../../assets/processed/Outer Wilds.png";
 import "./FrontPageGrid.css";
 import { Paper } from "@mui/material";
 import { styled } from "@mui/material/styles";
+import {
+  HERO_IMAGE,
+  MASK_IMAGE,
+  OUTER_WILDS_IMAGE,
+  TUNIC_IMAGE,
+} from "@/images";
+import { memo } from "react";
 
 const Root = styled(Paper)(() => ({
   border: "none",
@@ -19,22 +24,37 @@ type ArticleProps = {
   footer?: string;
 };
 
-const Article = ({ title, imageSrc, text, footer }: ArticleProps) => {
+const Article = memo(({ title, imageSrc, text, footer }: ArticleProps) => {
+  const paras = (text ?? "").trim().split(/\n+/g);
+
   return (
     <Root>
       <div className="frontpage-article">
         {title && <h1 style={{ textAlign: "center" }}>{title}</h1>}
         <img
           src={imageSrc}
-          style={{ maxWidth: "100%", filter: "grayscale(1) sepia(1)" }}
+          style={{
+            maxWidth: "100%",
+            maskImage: `url(${MASK_IMAGE})`,
+            maskMode: "luminance",
+            maskSize: "100% 100%",
+            maskRepeat: "no-repeat",
+            filter: "grayscale(1) sepia(1)",
+          }}
           alt=""
         />
-        {text && <p style={{ textAlign: "justify" }}>{text}</p>}
+        {text &&
+          paras.map((para) => <p style={{ textAlign: "justify" }}>{para}</p>)}
         {footer && <p style={{ textAlign: "right" }}>{footer}</p>}
       </div>
     </Root>
   );
-};
+});
+
+const HERO_TEXT = `
+In all of jujutsu sorcerer history, there's never been anyone who has managed to subjugate this one. In this exclusive issue, with this sacred treasure, we summon 24 year old, Divergent Sila Divine General, Aryan Pingle.
+
+While others coward behind platitudes of fake humility, he boldly proclaims himself to be a 'Web Developer Extraordinaire'. But don't be fooled by the sheer magnificence of his aptitude in Front-End Web Development, for his peers deem him a master of adaptability; he can make container management scripts as skillfully as he can prototype in Figma. This charismatic prodigy is the single greatest asset to the fields of Compiler Construction, Meta-programming, and Engineering in general (with the sole exception of Terry Davis, of course).`;
 
 export const FrontPageGrid = () => {
   return (
@@ -43,24 +63,20 @@ export const FrontPageGrid = () => {
       spacing={0}
       sx={{ marginTop: "0.5rem" }}
     >
-      <Article imageSrc={outerWildsImage} />
+      <Article imageSrc={OUTER_WILDS_IMAGE} />
       <Article
         title="Inside the mind of a genius"
-        imageSrc={image}
-        text="Like him or hate him — you gotta love Aryan Pingle. With his irreverant sense of humour and sheer aptitude for engineering, the charismatic web developer from Thane has captured the hearts and loins of many. Not to mention, he's mad cute."
+        imageSrc={HERO_IMAGE}
+        text={HERO_TEXT}
       />
       <Article
         title="Seven dead in fishing accident"
         text="To be completely honest, this article is fake. I dunno, it just sounded like a cool headline that would realistically be on the front page of an old-timey newspaper. I like theatricality so sue me. Haha JK, don't though. I'm being for real. If you're reading this you're legally not allowed to sue me. Get fukt."
       />
-      <Article
-        title="Aura."
-        imageSrc={image}
-        text="With the software developer's forway into corporate. there's only question on everyone's mind — what will the young prodigy do next?"
-      />
+      <Article imageSrc={TUNIC_IMAGE} />
       <Article
         title="Greatest of all time?"
-        imageSrc={image}
+        imageSrc={HERO_IMAGE}
         text="Aryan Pingle. Y'all know him, y'all motherfucking love him. But the question is — is he the GOAT?"
       />
     </Masonry>
